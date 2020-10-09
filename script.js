0d20385b92935f87c14401ed6834364019fa11c4
Julian Xu <julian.xu@PRGA-008842.local>
2020-10-09 12:48:33 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.ipm-selector__button');
+
+const handleCommunication = () => {
+  console.log(123);
+  alert(123);
+};
+
+button.addEventListener('click', handleCommunication);
