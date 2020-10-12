bf5ef72bd90ef0de93487e4e4ede377f96046410
Julian Xu <julian.xu@PRGA-008842.local>
2020-10-12 14:41:52 +0200

    first commit

---
+++ constants/templates.js
@@ -0,0 +1,34 @@
+const TEMPLATE_LIST = [
+  {
+    product: 'Avast One',
+    platform: 'Win',
+    style: 'Dark',
+    element: 'Toaster',
+    imgSrc: './img/toaster_1.png',
+    id: 353,
+  },
+  {
+    product: 'Avast One',
+    platform: 'Win',
+    style: 'Dark',
+    element: 'Toaster',
+    imgSrc: './img/toaster_2.png',
+    id: 354,
+  },
+  {
+    product: 'Avast One',
+    platform: 'Win',
+    style: '',
+    element: 'Toaster',
+    imgSrc: './img/toaster_3.png',
+    id: 355,
+  },
+  {
+    product: 'Avast One',
+    platform: 'Win',
+    style: 'Dark',
+    element: 'Toaster',
+    imgSrc: './img/toaster_4.png',
+    id: 356,
+  },
+];
